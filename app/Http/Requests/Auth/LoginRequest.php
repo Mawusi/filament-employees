8717ec67d9cc8db8325c505c495f1d9fa202b9ef
Fritz Mawusi <eabventures@gmail.com>
2022-07-18 15:09:59 +0000

    Checking the time again

--- app/Http/Requests/Auth/LoginRequest.php
+++ app/Http/Requests/Auth/LoginRequest.php
@@ -46,7 +46,7 @@ class LoginRequest extends FormRequest
         $this->ensureIsNotRateLimited();
 
         if (! Auth::attempt($this->only('email', 'password'), $this->boolean('remember'))) {
-            RateLimiter::hit($this->throttleKey(), 350);
+            RateLimiter::hit($this->throttleKey(), 450);
 
             throw ValidationException::withMessages([
                 'email' => trans('auth.failed'),
